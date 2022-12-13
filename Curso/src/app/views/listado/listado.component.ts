cbcd404ff5b3dfc72af7ed37ad6c4cb25956feec
Davcer99 <davidbalaguercervera@gmail.com>
2022-12-13 21:35:22 +0100

    Añadir componentes

--- Curso/src/app/views/listado/listado.component.ts
+++ Curso/src/app/views/listado/listado.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Entrada } from 'src/app/shared/interfaces';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
   styleUrls: ['./listado.component.css']
 })
-export class ListadoComponent {
+export class ListadoComponent implements OnInit {
+  public listadoEntradas: Entrada[];
 
+  constructor(){
+    this.listadoEntradas=[
+      {
+        titulo:'Introducción a Angular',
+        resumen:'En esta lección realizaremos una pequeña introducción '
+      },
+      {
+        titulo:'Typescript como lenguaje para Angular',
+        resumen: 'Breve recorrido por el lenguaje de Typescript'
+      },
+      {
+        titulo: 'Componentes en Angular',
+        resumen: 'Aprenderemos a usar los componentes en Angular'
+      }
+    ];
+  }
+
+  ngOnInit(): void {
+  }
+
+  /**
+   * mostrarTitulo
+   */
+  public mostrarTitulo( titulo:string):void {
+    alert(`Entrada seleccionada: ${titulo}.`);
+  }
 }
